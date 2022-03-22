697bfd4e808101a2c7b7a7ab50ddecc16be8457a
Ptshah <98435326+Ptshah@users.noreply.github.com>
2022-03-22 16:41:11 -0400

    Helper

--- Server/helper.js
+++ Server/helper.js
@@ -70,9 +70,44 @@ const signUpHandler = async (req, res, User) => {
   });
 };
 
+const signInHandler =async (req, res, User) => {
+  const { email, password } = req.body;
+  console.log({ email, password });
+  if (!email || !password)
+    res.status(400).json({ error: "The request was not properly formed" });
+  if (!password.length > 8)
+    res.status(400).json({ error: "The request was not properly formed" });
+  if (password.includes(" "))
+    res.status(400).json({ error: "The request was not properly formed" });
+
+  let isUpperCase = false;
+  let isLowerCase = false;
+  let notCharacter = false;
+  var format = /^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/;
+  var i = 0;
+  var character = "";
+  while (i <= password.length) {
+    character = password.charAt(i);
+    if (password.match(format)) {
+      notCharacter = true;
+    } else {
+      if (character == character.toUpperCase()) isUpperCase = true;
+      if (character == character.toLowerCase()) isLowerCase = true;
+    }
+    i++;
+  }
+  if (!isLowerCase || !isUpperCase)
+    res.status(400).json({ error: "The request was not properly formed" });
+
+  const userLogin = await User.findOne({ email: email, password: password });
+
+  if (userLogin)
+    res.status(200).json({ message: "User is Authorized", userLogin });
+  else res.status(401).json({ error: "User is not Authorized" });
+};
 
 module.exports = {
-  
+  signUpHandler,
   emailValidate,
   validatePassword,
   signInHandler,
