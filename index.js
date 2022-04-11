05c9238d47165c651e188a172b8593b3fe9b7a75
Ptshah <98435326+Ptshah@users.noreply.github.com>
2022-04-11 12:49:41 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -25,3 +25,47 @@ const index = () => {
 
   useEffect(() => {
   
+    setPageNo(1);
+    getArticles(1);
+  }, [category]);
+
+  function parseISOString(s) {
+    var b = s.split(/\D+/);
+    return new Date(Date.UTC(b[0], --b[1], b[2], b[3], b[4], b[5], b[6]));
+  }
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.articles}>
+        {articles !== null &&
+          articles.map((item, index) => (
+            <div className={styles.article} key={index}>
+              <h3>{item.title}</h3>
+              <span>{`${parseISOString(item.publishedAt).getFullYear()}-${
+                parseISOString(item.publishedAt).getMonth() + 1
+              }-${parseISOString(item.publishedAt).getDate()}`}</span>
+              <p>{item.description}</p>
+            </div>
+          ))}
+      </div>
+      <div className={styles.pagination}>
+        <BsChevronLeft className={styles.icon} />
+        {_.times(totalPages, (i) => (
+          <span
+            key={i}
+            className={pageNo == i + 1 && styles.active}
+            onClick={() => {
+              setPageNo(i + 1);
+              getArticles(i + 1);
+            }}
+          >
+            {i + 1}
+          </span>
+        ))}
+        <BsChevronRight className={styles.icon} />
+      </div>
+    </div>
+  );
+};
+
+export default index;
